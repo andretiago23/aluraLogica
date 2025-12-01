5a2904bab1bf1adca032b86bd33a5a57e24cc21a
andretiago23 <tiagoclossfilho@gmail.com>
2025-12-01 11:44:07 -0400

    terminando campos

--- app.js
+++ app.js
@@ -1,18 +1,51 @@
-let numeroSecreto = geraNumeroAleatorio();
 
-function exibiTextoNaTela(tag, texto) {
-    let campo = document.querySelector(tag, texto);
+let contagem = 1
+let chute
+let numeroSecreto = gerarNumeroAleatorio();
+let titulo = document.querySelector('h1');
+let paragrafo = document.querySelector('p');
+
+// console.log("paragrafo", paragrafo)
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibiTextoNaTela('h1', ' jogo do número secreto ')
-exibiTextoNaTela('p', 'Escolha um número entre 1 e 10');
-
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+// exibirMensagemInicial()
 function verificarChute() {
-    console.log('o botao foi clicado!');
+    let chute = document.querySelector('input').value;
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('p', 'Você descobriu o número secreto com ' + contagem + ' tentativas');
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }
+    else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor que ' + chute);
+        }
+        else {
+            exibirTextoNaTela('p', 'O número secreto é maior que ' + chute);
+        }
+        contagem++
+        limparCampo();
+    }
+}
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = ' ';
+}
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    contagem = 1;
+    exibirMensagemInicial();
 }
 
-function geraNumeroAleatorio() {
-    return parseInt(Math.random() * x + 1);
-
-}
