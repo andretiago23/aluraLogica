7d51430bea07d397b35687fb061f2b91d295f17a
andretiago23 <tiagoclossfilho@gmail.com>
2025-12-03 21:11:14 -0400

    tentando resolver sozinho

--- licao.js
+++ licao.js
@@ -1,21 +1,15 @@
-// const despesas = [1200, 10, 2000, 800]; //lista
+ const despesas = [1, 2, 3, 4, 5];
 
 
-// despesas.map((numeroLimite, i) => { })
 
-let total = 0 //total da somatoria
 
-//função que guarda o 'for'
-function somaArray(numeros) {
-    //ler o proximo item
-    for (let i = 0; i < numeros.length; i++) {     //enquanto o item percorrido for menor que o tamanho da lista 
-        let despesaAtual = numeros[i]   //cria uma variavel para o item que está sendo lido
+function somaList(despesas) {
+   for (let i = 0; i < despesas.length; i++ ){
+      let numeroA = despesas[i];
+     let total = 0;    
+      total = numeroA + total
+   }
 
-        total = despesaAtual + total
-
-    }
-
-    console.log(`O total é ${total}`)//esse aqui manda exibir a mensagem na tela 
 }
-//aqui ele chama  a função qu foi mencionada lá em cima
-somaArray([1200, 10, 2000, 50, 23, 120, 123])
+somaList()
+console.log('total: ' + total)
---
+++ ms.js
@@ -0,0 +1,6 @@
+const arrNumeros = [1 ,2 ,3 ,4]
+
+ for (let i = 0; i <arrNumeros.length; i++) {
+    arrNumeros[i] = arrNumeros[i] *10
+ }   
+ console.log(arrNumeros)
